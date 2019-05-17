8ffe8eefa59c44eb81b9b0fb52be131934a5b599
Julien Jerphanion (@jjerphan) <git@jjerphan.xyz>
2019-05-17 14:38:30 +0200

    [DEBUG] Add logs for Client

--- distributed/client.py
+++ distributed/client.py
@@ -594,6 +594,7 @@ class Client(Node):
         direct_to_workers=None,
         **kwargs
     ):
+        logging.info("Client.__init__ called")
         if timeout == no_default:
             timeout = dask.config.get("distributed.comm.timeouts.connect")
         if timeout is not None:
@@ -718,6 +719,7 @@ class Client(Node):
     @classmethod
     def current(cls):
         """ Return global client if one exists, otherwise raise ValueError """
+        logging.info("Client.current called")
         return default_client()
 
     @property
@@ -844,6 +846,7 @@ class Client(Node):
 
     def start(self, **kwargs):
         """ Start scheduler running in separate thread """
+        logging.info("Client.start called")
         if self.status != "newly-created":
             return
 
@@ -889,6 +892,7 @@ class Client(Node):
 
     @gen.coroutine
     def _start(self, timeout=no_default, **kwargs):
+        logging.info("Client._start called")
         if timeout == no_default:
             timeout = self._timeout
         if timeout is not None:
@@ -1270,6 +1274,7 @@ class Client(Node):
         --------
         Client.restart
         """
+        logging.info("Client.close called")
         if timeout == no_default:
             timeout = self._timeout * 2
         # XXX handling of self.status here is not thread-safe
@@ -1359,6 +1364,7 @@ class Client(Node):
         --------
         Client.map: Submit on many arguments at once
         """
+        logging.info("Client.submit called")
         if not callable(func):
             raise TypeError("First input to submit must be a callable function")
 
@@ -2365,6 +2371,7 @@ class Client(Node):
 
         >>> c.run(print_state, wait=False)  # doctest: +SKIP
         """
+        logging.info("Client.run called")
         return self.sync(self._run, function, *args, **kwargs)
 
     def run_coroutine(self, function, *args, **kwargs):
@@ -2409,6 +2416,7 @@ class Client(Node):
         fifo_timeout=0,
         actors=None,
     ):
+        logging.info("Client._graph_to_futures called")
         with self._refcount_lock:
             if resources:
                 resources = self._expand_resources(
@@ -2478,8 +2486,7 @@ class Client(Node):
             if isinstance(retries, Number) and retries > 0:
                 retries = {k: retries for k in dsk3}
 
-            self._send_to_scheduler(
-                {
+            request = {
                     "op": "update-graph",
                     "tasks": valmap(dumps_task, dsk3),
                     "dependencies": dependencies,
@@ -2494,7 +2501,7 @@ class Client(Node):
                     "fifo_timeout": fifo_timeout,
                     "actors": actors,
                 }
-            )
+            self._send_to_scheduler(request)
             return futures
 
     def get(
